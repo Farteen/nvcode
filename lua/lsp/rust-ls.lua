1adbd9db362013c1a0de93a56a81edc96e17426e
Farteen <xujavacn@gmail.com>
2021-04-05 22:45:06 +0800

    change colorscheme and add rust language documentation snippets

--- lua/lsp/rust-ls.lua
+++ lua/lsp/rust-ls.lua
@@ -1,5 +1,32 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 require'lspconfig'.rust_analyzer.setup{
     cmd = {DATA_PATH .. "/lspinstall/rust/rust-analyzer"},
-    on_attach = require'lsp'.common_on_attach
+    on_attach = require'lsp'.common_on_attach,
+	settings = {
+		["rust-analyzer"] = {
+			assist = {
+                importMergeBehavior = "last",
+                importPrefix = "by_self",
+            },
+            cargo = {
+                loadOutDirsFromCheck = true
+            },
+            procMacro = {
+                enable = true
+            },
+		}
+	},
+	capabilities = capabilities,
+    handlers = {
+        ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+            virtual_text = true,
+            signs = true,
+            underline = true,
+            update_in_insert = true
+        })
+    }
+
 }
 
